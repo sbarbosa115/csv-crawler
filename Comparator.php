5524abdd29c7fe902406f07fb2373d5993ad58a8
s-admpubco <s-admpubco@s-admpubco-PC>
2014-01-16 10:38:32 -0500

    Correction code

--- Comparator.php
+++ Comparator.php
@@ -1,38 +1,166 @@
 <?php
 
 /**
- * Make tests to of domains and returns all error codes and the IP of each of the domains.
+ * Testing URL Class for multiple websites from csv file.
+ * @version 1.0
  * @author Sergio Barbosa <sbarbosa115@gmail.com>
+ * @package TesterCrawler
  */
-set_time_limit(0);
-ini_set("memory_limit", "512M");
+class TesterCrawler {
 
-class Comparator {
+    protected $file = '';
+    protected $tempFileName = '';
+    protected $homeSites = array('index.html', 'index.htm', 'home.htm', 'home.html');
 
-    protected $file = 'lista_dominios.txt';
-    protected $tempFileName;
-    protected $urlFile;
-
-    function __construct() {
+    function __construct($file) {
+        set_time_limit(0);
         $this->tempFileName = 'list_review_domains_' . time() . '.txt';
+        $this->file = $file;
     }
 
     /**
-     * Read file from provided location.
-     * @return null 
+     * Read file from provides location.
+     * @return null
      */
-    public function readFile() {
+    protected function readFile() {
         $fileWriteResults = fopen($this->tempFileName, 'wb');
+        fwrite($fileWriteResults, "IP;DOMINIO;RECURSO;CODIGO RESPUESTA;\n");
+        fclose($fileWriteResults);
+
         if (($handle = fopen($this->file, "r")) !== FALSE) {
             while ($data = fgetcsv($handle, 0, ";")) {
-                $domain = explode('/', $data[0]);
-                $ip = $this->getIpAddress($domain[0]);
-                $result = $this->verifyUrlResource($data[0]);
-                fwrite($fileWriteResults, $data[0] . ';' . $result . ';' . $ip . "\n");
+                $url = $this->addProtocol($data[0]);
+                if ($url) {
+                    $this->getDomainData($url);
+                }
             }
         }
         fclose($handle);
+    }
+
+    /**
+     * Get and create domain data file.
+     * @param string $url Url to get data.
+     * @return null
+     */
+    protected function getDomainData($url = 'http://www.example.com/') {
+        $fileWriteResults = fopen($this->tempFileName, 'a');
+        $bag = $linksBag = array();
+        $flag = false;
+
+        $dataUrl = $this->verifyUrlResource($url);
+        $doc = $this->getDocumentCurl($dataUrl['data']);
+
+        $links = $doc->getElementsByTagName('a');
+        foreach ($links as $tag) {
+            if (!in_array($tag->getAttribute('href'), $bag)) {
+                $linksBag[] = $this->validateUrl($url, $tag->getAttribute('href'));
+                $bag[] = $tag->getAttribute('href');
+            }
+        }
+
+        $img = $doc->getElementsByTagName('img');
+        foreach ($img as $tag) {
+            if (!in_array($tag->getAttribute('src'), $bag)) {
+                $linksBag[] = $this->validateUrl($url, $tag->getAttribute('src'));
+                $bag[] = $tag->getAttribute('src');
+            }
+        }
+        
+        print_r($img);
+        print_r($links);
+        print_r($bag);
+        var_dump($dataUrl);
+        die;
+
+        foreach ($this->homeSites as $home) {
+            $domainStatus = $this->verifyUrlResource($url . '/' . $home);
+            if ($domainStatus['code'] >= 200 && $domainStatus['code'] < 400) {
+                $doc = $this->getDocument($url . '/' . $home);
+
+                $links = $doc->getElementsByTagName('a');
+                foreach ($links as $tag) {
+                    if (!in_array($tag->getAttribute('href'), $bag)) {
+                        echo $tag->getAttribute('href');
+                        $linksBag[] = $this->validateUrl($url, $tag->getAttribute('href'));
+                        $bag[] = $tag->getAttribute('href');
+                    }
+                }
+
+                $img = $doc->getElementsByTagName('img');
+                foreach ($img as $tag) {
+                    if (!in_array($tag->getAttribute('src'), $bag)) {
+                        echo $tag->getAttribute('src');
+                        $linksBag[] = $this->validateUrl($url, $tag->getAttribute('src'));
+                        $bag[] = $tag->getAttribute('src');
+                    }
+                }
+
+                for ($i = 0; $i <= sizeof($linksBag) - 1; $i++) {
+                    if ($url === substr($linksBag[$i], 0, strlen($url))) {
+                        $resourceStatus = $this->verifyUrlResource($linksBag[$i]);
+                        $lineResult = $resourceStatus['ip'] . ';' . $linksBag[$i] . ';' . $resourceStatus['code'] . "\n";
+                        fwrite($fileWriteResults, $lineResult);
+                    }
+                }
+                $flag = true;
+            }
+        }
+
+        if (!$flag) {
+            $resourceStatus = $this->verifyUrlResource($url);
+            $lineResult = $resourceStatus['ip'] . ';' . $url . ';' . $resourceStatus['code'] . "\n";
+            fwrite($fileWriteResults, $lineResult);
+        }
+
         fclose($fileWriteResults);
+    }
+
+    protected function validateUrl($originalUrl, $urlResource) {
+        $url = '';
+        if (!preg_match("/(ftp|https|http?)/", $urlResource)) {
+            $url = $originalUrl . '/' . $urlResource;
+        } else {
+            $url = $urlResource;
+        }
+        return $url;
+    }
+
+    /**
+     * Add http protocol to url before process.
+     * @param string $url Url to process.
+     * @return string Url repared.
+     */
+    protected function addProtocol($url) {
+        if ($url) {
+            if (substr($url, 0, strlen('http://')) !== 'http://' || substr($url, 0, strlen('https://')) !== 'https://') {
+                return 'http://' . $url;
+            } else {
+                return $url;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get the html file title.
+     * @return null
+     */
+    protected function getDocumentCurl($data) {
+        $doc = new DOMDocument();
+        @$doc->loadHTML($data);
+        return $doc;
+    }
+
+    /**
+     * Get the html file title.
+     * @return null
+     */
+    protected function getDocument($url) {
+        $doc = new DOMDocument();
+        @$doc->loadHTMLFile($url);
+        return $doc;
     }
 
     /**
@@ -43,26 +171,26 @@ class Comparator {
     protected function verifyUrlResource($url) {
         $handle = curl_init($url);
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($handle, CURLOPT_TIMEOUT, 2);
+        curl_setopt($handle, CURLOPT_TIMEOUT, 10);
+        curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
 
-        $response = curl_exec($handle);
+        $data = curl_exec($handle);
         $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+        $ip = curl_getinfo($handle, CURLINFO_PRIMARY_IP);
 
         curl_close($handle);
-        return $httpCode;
+        return array('code' => $httpCode, 'ip' => $ip, 'data' => $data);
     }
 
     /**
-     * Get ip address from url.
-     * @param string $url Url to get ip address.
-     * @return string Ip address from url.
+     * Read file from provides location.
+     * @return null
      */
-    protected function getIpAddress($url) {
-        $url = preg_replace(array('#^http?://#', '#/#', '#^http?://#'), '', $url);
-        return gethostbyname($url);
+    public function processDomains() {
+        $this->readFile();
     }
 
 }
 
-$run = new Comparator();
-$run->readFile();
+$crawler = new TesterCrawler('lista_dominios.txt');
+$crawler->processDomains();
---
+++ CrawlerSimple.php
@@ -0,0 +1,149 @@
+<?php
+
+/**
+ * Testing URL Class for multiple websites from csv file.
+ * @version 1.0
+ * @author Sergio Barbosa <sbarbosa115@gmail.com>
+ * @package TesterCrawler
+ */
+class TesterCrawler {
+
+    protected $file = '';
+    protected $tempFileName = '';
+    protected $homeSites = array('index.html', 'index.htm', 'home.htm', 'home.html');
+
+    /**
+     * Contruct class
+     * @param string $file Name of archivo to process.
+     * @return null
+     * @author Sergio Barbosa <sbarbosa115@gmail.com>
+     */
+    function __construct($file) {
+        set_time_limit(0);
+        $this->tempFileName = 'list_review_domains_' . time() . '.txt';
+        $this->file = $file;
+    }
+
+    /**
+     * Read file from provides location.
+     * @return null
+     * @author Sergio Barbosa <sbarbosa115@gmail.com>
+     */
+    protected function readFile() {
+        $fileWriteResults = fopen($this->tempFileName, 'wb');
+        fwrite($fileWriteResults, "IP;DOMINIO;TITULO;DESCRIPCION;CODIGO RESPUESTA;PESO\n");
+        fclose($fileWriteResults);
+
+        if (($handle = fopen($this->file, "r")) !== FALSE) {
+            while ($data = fgetcsv($handle, 0, ";")) {
+                $url = $this->addProtocol($data[0]);
+                if ($url) {
+                    $this->getDomainData($url);
+                }
+            }
+        }
+        fclose($handle);
+    }
+
+    /**
+     * Get and create domain data file.
+     * @param string $url Url to get data.
+     * @return null
+     * @author Sergio Barbosa <sbarbosa115@gmail.com>
+     */
+    protected function getDomainData($url) {
+        if ($url) {
+            $fileWriteResults = fopen($this->tempFileName, 'a');
+            $dataUrl = $this->verifyUrlResource($url);
+            $doc = $this->getDocumentCurl($dataUrl['data']);
+            $nodes = $doc->getElementsByTagName('title');
+            $metas = $doc->getElementsByTagName('meta');
+            $description = '';
+
+            foreach ($metas as $meta) {
+                if (strtolower($meta->getAttribute('name')) == 'description') {
+                    if ($meta->getAttribute('content')) {
+                        $description = $meta->getAttribute('content');
+                    } else if ($meta->getAttribute('value')) {
+                        $description = $meta->getAttribute('value');
+                    }
+                }
+            }
+
+            $lineResult = $dataUrl['ip'] . ';' . $url . ';' . $this->cleanString($nodes->item(0)->nodeValue) . ';' . $this->cleanString($description) . ';' . $dataUrl['code'] . ';' . $dataUrl['size'] . "\n";
+            fwrite($fileWriteResults, $lineResult);
+            fclose($fileWriteResults);
+        }
+    }
+
+    /**
+     * Clean string to print in file.
+     * @param string $string String to clean.
+     * @return string String clean.
+     */
+    protected function cleanString($string) {
+        return utf8_decode(substr(trim($string), 0, 20));
+    }
+
+    /**
+     * Add http protocol to url before process.
+     * @param string $url Url to process.
+     * @return string Url repared.
+     * @author Sergio Barbosa <sbarbosa115@gmail.com>
+     */
+    protected function addProtocol($url) {
+        if ($url) {
+            if (substr($url, 0, strlen('http://')) !== 'http://' || substr($url, 0, strlen('https://')) !== 'https://') {
+                return 'http://' . $url;
+            } else {
+                return $url;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get the html file title.
+     * @return null
+     * @author Sergio Barbosa <sbarbosa115@gmail.com>
+     */
+    protected function getDocumentCurl($data) {
+        $doc = new DOMDocument();
+        @$doc->loadHTML($data);
+        return $doc;
+    }
+
+    /**
+     * Get http code for the url.
+     * @param string $url Url to get http code
+     * @return int Code returned. 
+     * @author Sergio Barbosa <sbarbosa115@gmail.com>
+     */
+    protected function verifyUrlResource($url) {
+        $handle = curl_init($url);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($handle, CURLOPT_TIMEOUT, 10);
+        curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
+        $data = curl_exec($handle);
+        $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+        $ip = curl_getinfo($handle, CURLINFO_PRIMARY_IP);
+        $size = curl_getinfo($handle, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+
+        curl_close($handle);
+        return array('code' => $httpCode, 'ip' => $ip, 'data' => $data, 'size' => $size);
+    }
+
+    /**
+     * Read file from provides location.
+     * @return null
+     * @author Sergio Barbosa <sbarbosa115@gmail.com>
+     */
+    public function processDomains() {
+        $this->readFile();
+    }
+
+}
+
+$crawler = new TesterCrawler('lista_dominios.txt');
+$crawler->processDomains();
